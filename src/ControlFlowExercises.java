b940daa6dd9f829b6926f4c7c35957e05a66cd2b
Reginald Graham <reg.graham98@gmail.com>
2023-02-10 11:23:18 -0600

    java exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,20 @@
 public class ControlFlowExercises {
     public static void main(String[] arg){
+//    Integer i=5;
+//        System.out.println(i);
+//        while (i<=15){
+//            System.out.println("i is " + i);
+//            i++;
+//        }
+    long o=2;
+//        System.out.println(o);
+        do{
+            System.out.println("o is "+ o);
+           o *= o;
+        }while (o<=1000000);
 
+        for(long y=2; y<100000;y*=y){
+            System.out.println("y is: " +y);
+        }
     }
 }
