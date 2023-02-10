f0dc577bc957d74076dc041af1e3034ad0660ede
Reginald Graham <reg.graham98@gmail.com>
2023-02-10 10:42:42 -0600

    java exercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ConsoleExercises {
+
+    public static void main(String[] arg){
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately "+"%.2f%n",pi);
+//
+//        Scanner number= new Scanner(System.in);
+//
+//        System.out.println("enter a number");
+//
+//        Integer numberInt=number.nextInt();
+//
+//        System.out.printf("here's your number: %s", numberInt);
+//
+//        Scanner word= new Scanner(System.in);
+////
+//        System.out.println("enter three words seperated by a space");
+//
+//        String wordEntered=word.nextLine();
+//        String[] wordSplit=wordEntered.split(" ");
+//
+//
+//        System.out.println(wordSplit[0]);
+//        System.out.println(wordSplit[1]);
+//        System.out.println(wordSplit[2]);
+
+//        Scanner sentence= new Scanner(System.in);
+//
+//        System.out.println("tell me something i want to hear");
+//
+//        String sentenceRepeat= sentence.nextLine();
+//
+//        System.out.println(sentenceRepeat);
+////
+//        Scanner roomSize=new Scanner(System.in);
+//        System.out.println("what is the height?");
+//        String height= roomSize.nextLine();
+//        System.out.println("whats the width?");
+//        String width=roomSize.nextLine();
+//        System.out.println("whats the length?");
+//        String length=roomSize.nextLine();
+//
+////            ONLY ACCEPTS WHOLE NUMBERS
+////        System.out.printf("area is: %s ",Integer.parseInt(height)* Integer.parseInt(width));
+////        System.out.printf("perimeter is: %s", Integer.parseInt(height)*2+Integer.parseInt(width)*2);
+//
+////        ACCEPTS DECIMALS
+//        System.out.printf("area is: %.2f%n ",Double.parseDouble(height)* Double.parseDouble(width));
+//        System.out.printf("perimeter is: %.2f%n ", Double.parseDouble(height)*2+Double.parseDouble(width)*2);
+//        System.out.printf("volume is: %.2f", Double.parseDouble(length)*Double.parseDouble(width)*Double.parseDouble(height));
+////
+    }
+
+}
